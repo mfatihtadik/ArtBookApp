64bf16850516b3b9d34f9a89a6d761cba7b6069c
fatih <fatihtadk@gmail.com>
2025-07-16 21:45:45 +0300

    Room Database Added.

--- app/src/main/java/com/mftadik/artbooktesting/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/mftadik/artbooktesting/view/ArtDetailsFragment.kt
@@ -2,12 +2,40 @@ package com.mftadik.artbooktesting.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.mftadik.artbooktesting.R
+import com.mftadik.artbooktesting.databinding.FragmentArtDetailsBinding
+import com.mftadik.artbooktesting.databinding.FragmentArtsBinding
 
 class ArtDetailsFragment : Fragment(R.layout.fragment_art_details) {
 
+    private var fragmentBinding : FragmentArtDetailsBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentArtDetailsBinding.bind(view)
+        fragmentBinding = binding
+
+        binding.artImageView.setOnClickListener {
+            findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
+        }
+
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+
+    }
+
+    override fun onDestroyView() {
+        fragmentBinding = null
+        super.onDestroyView()
     }
 }
