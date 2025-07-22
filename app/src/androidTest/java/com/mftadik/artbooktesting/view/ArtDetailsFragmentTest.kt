29948d605fad610d453866885a9ba55dfeefbb41
fatih <fatihtadk@gmail.com>
2025-07-23 01:02:28 +0300

    Ui Test added.

---
+++ app/src/androidTest/java/com/mftadik/artbooktesting/view/ArtDetailsFragmentTest.kt
@@ -0,0 +1,68 @@
+package com.mftadik.artbooktesting.view
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.filters.MediumTest
+import com.mftadik.artbooktesting.R
+import com.mftadik.artbooktesting.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+import javax.inject.Inject
+
+@MediumTest
+@HiltAndroidTest
+@ExperimentalCoroutinesApi
+class ArtDetailsFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory : ArtFragmentFactory
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun testNavigationFromArtDetailsToImageAPI () {
+
+        val navController = Mockito.mock(NavController::class.java)
+
+        launchFragmentInHiltContainer<ArtDetailsFragment>(factory = fragmentFactory) {
+            Navigation.setViewNavController(requireView(),navController)
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.artImageView)).perform(click())
+        Mockito.verify(navController).navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
+    }
+
+    @Test
+    fun testOnBackPressed() {
+        val navController = Mockito.mock(NavController::class.java)
+
+        launchFragmentInHiltContainer<ArtDetailsFragment>(factory = fragmentFactory) {
+            Navigation.setViewNavController(requireView(),navController)
+        }
+
+        Espresso.pressBack()
+        Mockito.verify(navController).popBackStack()
+
+    }
+
+
+
+}
